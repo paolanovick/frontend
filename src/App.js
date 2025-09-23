2878980bbdec543e2571d2086541d96e45bd28a8
Paola Novick <paola.novick@davinci.edu.ar>
2025-09-23 19:07:00 -0300

    armado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CheckinList from "./components/CheckinList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Recordatorios de Check-in
+      </h1>
+      <CheckinList csvFilePath="/vuelos.csv" />
     </div>
   );
 }
